23551ada64081a039e0b7c9a1e197e37311c3478
abhilashchaparala <abhilashchaparala81@gmail.com>
2021-09-04 19:56:57 +0530

    added mean_median_mode function

--- featureEngineering/handling_missing_values/missing_values.py
+++ featureEngineering/handling_missing_values/missing_values.py
@@ -9,4 +9,70 @@ class missingValues:
         self.logger = App_logger()
     
     
+    def find_mean(self,mean_meadian_mode,col_name):
+        """
+        This class is used for replacing the missing values of a column of the data with mean:
+
+        Written By: Abhilash Chaparala
+        Version: 1.0
+        Revisions: None
+        """
+        
+        try:
+            
+            if mean_meadian_mode == 'mean':
+                self.data[col_name].fillna(self.data.col_name.mean())
+                self.logger.info('column {}\'s missing values are replaced with {}'.format(col_name,mean_meadian_mode))
+                return True
+        except:
+            
+            self.logger.error('column {}\'s missing values are not replaced with {}'.format(col_name,mean_meadian_mode))
+            return False
+        
+    
+    
+    def find_median(self,mean_meadian_mode,col_name):
+        """
+        This class is used for replacing the missing values of a column of the data with median:
+
+        Written By: Abhilash Chaparala
+        Version: 1.0
+        Revisions: None
+        """
+        try:
+            
+            if mean_meadian_mode == 'median':
+                
+                self.data[col_name].fillna(self.data.col_name.median())
+                self.logger.info('column {}\'s missing values are replaced with {}'.format(col_name,mean_meadian_mode))
+                return True
+        
+        except:
+            self.logger.error('column {}\'s missing values are not replaced with {}'.format(col_name,mean_meadian_mode))
+            return False   
+    
+    
+    def find_mode(self,mean_meadian_mode,col_name):
+        """
+        This class is used for replacing the missing values of a column of the data with mode:
+
+        Written By: Abhilash Chaparala
+        Version: 1.0
+        Revisions: None
+        """
+        try:
+            
+            if mean_meadian_mode == 'mode':
+                self.data[col_name].fillna(self.data.col_name.mode())
+                self.logger.info('column {}\'s missing values are replaced with {}'.format(col_name,mean_meadian_mode))
+                return True
+        
+        except:
+            self.logger.error('column {}\'s missing values are not replaced with {}'.format(col_name,mean_meadian_mode))
+            return False
+        
+        
+
+
 if __name__ == "__main__":
+    pass
