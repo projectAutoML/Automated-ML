153afe88e63cf38856831daf00244e67af8fbe4f
abhilashchaparala <abhilashchaparala81@gmail.com>
2021-09-04 21:06:59 +0530

    added better logger function

--- Application_logs/logger.py
+++ Application_logs/logger.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import logging
 """
     This class or package is help to logs each and every
     informations.
@@ -14,4 +14,25 @@ class App_logger:
         self.current_time = self.now.strftime("%H:%M:%S")
         file_object.write(
             str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message)
+def getLog(self,nm):
+        # Creating custom logger
+    logger = logging.getLogger(nm)
+        # reading contents from properties file
+    f = open("properties.txt", 'r')
+    if f.mode == "r":
+        loglevel = f.read()
+    if loglevel == "ERROR":
+        logger.setLevel(logging.ERROR)
+    elif loglevel == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+        # Creating Formatters
+    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+        # Creating Handlers
+    file_handler = logging.FileHandler('test.log')
+        # Adding Formatters to Handlers
+    file_handler.setFormatter(formatter)
+        # Adding Handlers to logger
+    logger.addHandler(file_handler)
+    return logger
 
+    
